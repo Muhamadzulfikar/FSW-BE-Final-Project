77db23f3599e706cf8b9fce8beb1c88c294b936f
Muhamad Zulfikar <muhamadzulfikar995@gmail.com>
2023-11-28 12:07:25 +0700

    feat: update routes

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ app.use('/v1', authRoute);
 
 app.use('/v1/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
+app.put('/v1/progress-course', userChapterModuleController.updateProgressChapter);
+
 app.get('/', (req, res) => {
   res.status(200).json({
     message: 'successfully',
   });
 });
-
-app.put('/v1/progress-course', userChapterModuleController.updateProgressChapter);
 
 // eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
