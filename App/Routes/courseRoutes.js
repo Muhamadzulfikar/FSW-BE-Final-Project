0b0ba102a2c6b7302612a3f0610a6b88f6113ca7
Muhamad Zulfikar <muhamadzulfikar995@gmail.com>
2023-11-28 11:39:35 +0700

    init: delete unused file in this branch

--- App/Routes/courseRoutes.js
+++ App/Routes/courseRoutes.js
@@ -1,11 +1,8 @@
 const express = require('express');
-const courseCategoryController = require('../Controllers/courseCategoryController');
 const courseController = require('../Controllers/courseController');
 
 const route = express.Router();
 
-route.get('/course-categories', courseCategoryController.getAllCourseCategory);
-route.get('/course-categories/:id', courseCategoryController.getCourseCategoryById);
 route.get('/courses', courseController.getAllCourses);
 route.get('/course/:id', courseController.getCourseDetailById);
 
