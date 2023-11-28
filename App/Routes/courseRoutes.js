aa3f85dc39d771d38457cb8d89381725e4830088
Muhamad Zulfikar <muhamadzulfikar995@gmail.com>
2023-11-28 11:01:10 +0700

    feat: add routes for course

--- App/Routes/courseRoutes.js
+++ App/Routes/courseRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const courseController = require('../Controllers/courseController');
 
 const route = express.Router();
 
-route.get('/courses');
+route.get('/courses', courseController.getAllCourses);
+route.get('/course/:id', courseController.getCourseDetailById);
 
 module.exports = route;
