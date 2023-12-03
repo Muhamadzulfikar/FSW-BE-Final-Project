b633fc09fd0a97d51a984a119ed8ae06156998bf
Muhamad Zulfikar <muhamadzulfikar995@gmail.com>
2023-12-03 14:10:28 +0700

    Revert "Merge pull request #43 from Muhamadzulfikar/api/progress-course"
    This reverts commit 4f1b6478bd3a9a4fca95cecea4d62904b455eb68, reversing
    changes made to 4d9dd393db8f3795d9cfc94e169696f50505c3d1.

---
+++ App/Controllers/authController.js
@@ -0,0 +1,32 @@
+const { v4: uuidv4 } = require('uuid');
+const authService = require('../Services/authService');
+const responseError = require('../Error/responseError');
+
+module.exports = {
+  async userLogin(req, res) {
+    try {
+      const { body, user } = req;
+      const login = await authService.userLogin(body.password, user);
+      res.status(200).json(login);
+    } catch (error) {
+      responseError(res, error);
+    }
+  },
+
+  async userRegister(req, res) {
+    try {
+      req.body.id = uuidv4();
+      if (req.user) {
+        req.body.role = 'admin';
+      }
+      const register = await authService.userRegister(req.body);
+      res.status(201).json({
+        status: 'Ok',
+        message: 'Successfully register',
+        data: register,
+      });
+    } catch (error) {
+      res.status(422).json(error);
+    }
+  },
+};
