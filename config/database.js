27ae1f8a1eed487afebc7d210d1e2b34613a74fb
Muhamad Zulfikar <muhamadzulfikar995@gmail.com>
2023-12-04 15:08:37 +0700

    feat: ignore unit test to production and setup db testing

--- config/database.js
+++ config/database.js
@@ -9,10 +9,10 @@ module.exports = {
     dialect: 'postgres',
   },
   test: {
-    username: 'postgres',
-    password: '0110',
-    database: 'DB_C4',
-    host: '127.0.0.1',
+    username: process.env.DB_USERNAME_LOCAL,
+    password: process.env.DB_PASSWORD_LOCAL,
+    database: process.env.DB_NAME_LOCAL,
+    host: process.env.DB_HOST_LOCAL,
     dialect: 'postgres',
   },
   production: {
